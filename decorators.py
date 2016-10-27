48d3f7f6f593ce5fdb9078234b08d74eb4a6fca5
krsm <krsm184@gmail.com>
2016-10-27 14:09:26 -0400

    simple study related to decorators

--- decorators.py
+++ decorators.py
@@ -3,26 +3,25 @@
 simple study of decorators
 
 """
+
 # First example
 def outerfunction(outer):
     def innerfunction(inner):
-        return outer(inner)+1
+        return outer(inner) + 1
     return innerfunction
 
 @outerfunction
 def addone(x):
-    return x+1
+    return x + 1
 
 # Second Example
 def wrapfunction(fn):
     print("Executing wra_function")
-
     def wrappedfunction():
         print("Calling %s inside wrappedfunction" % fn.__name__)
         # calling fn
         fn()
         print("Executed %s inside wrappedfunction" % fn.__name__)
-
     return wrappedfunction
 
 @wrapfunction
@@ -30,6 +29,7 @@ def tobedecorated():
     print("Executing tobedecorated function")
 
 if __name__ == "__main__":
-
+    #first example
     print(addone(5))
+    #second example
     tobedecorated()

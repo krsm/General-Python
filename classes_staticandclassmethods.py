1fe7547972c8a1a12e0c00fc85dca319affa6920
krsm <krsm184@gmail.com>
2017-01-20 21:49:03 -0500

    cleaning spaces

--- classes_staticandclassmethods.py
+++ classes_staticandclassmethods.py
@@ -9,50 +9,47 @@ import datetime
 # First Example
 
 class Warrior(object):
-    
+
     # delta position - class variable
     delta_position = 10
     #number of warriors
     num_of_warriors = 0
-    
-    
+
     def __init__(self,posx,posy):
         self.posx = posx
         self.posy = posy
-        
+
         Warrior.num_of_warriors+=1
-        
-        
+
     def move(self, posx,posy):
         self.posx = posx+ self.delta_position
         self.posy = posy +self.delta_position
-        
+
     def position(self):
         return '{} , {}' .format(self.posx,self.posy)
-        
+
 # Second Example
 
 class Monster(object):
-    
-    
+
     # delta position - class variable
     delta_position = 10
     #number of monters
     num_of_monsters = 0
-    
-    def __init__(self,posx,posy):
+
+    def __init__(self, posx, posy):
         self.posx = posx
         self.posy = posy
-        
-        Monster.num_of_monsters+=1
-        
+
+        Monster.num_of_monsters+= 1
+
     def move(self, posx,posy):
-        self.posx = posx+ self.delta_position
-        self.posy = posy +self.delta_position
-        
+        self.posx = posx + self.delta_position
+        self.posy = posy + self.delta_position
+
     def position(self):
         return '{} , {}' .format(self.posx,self.posy)
-     
+
     # used to change the value of class variable
     # same as Monster.delta_position = new_value
     @classmethod
@@ -61,53 +58,51 @@ class Monster(object):
 
     #using classmethod as alternatives consctructor
     @classmethod
-    def from_str(cls,pos_str):
+    def from_str(cls, pos_str):
         posx, posy = pos_str.split("-")
-        return cls(posx,posy)      
-       
-    #static methods
+        return cls(posx, posy)
+
+        #static methods
     #behaves like a regular function
     @staticmethod
     def is_gameday(day):
         if day.weekday == 5 or day.weekday == 6:
             return False
         return True
-            
-        
-        
+
 if __name__ == "__main__":
-    
+
     # First Example
-    
+
     # declare an instance of Warrior Class
-    Savage = Warrior(20,10)
-    Savage.move(10,25)
+    Savage = Warrior(20, 10)
+    Savage.move(10, 25)
     print(Savage.position())
-    
+
     # print namespace of instance
-    print (Savage.__dict__)
-    
+    print(Savage.__dict__)
+
     # print namespace of class Warrior
-    print (Warrior.__dict__)
+    print(Warrior.__dict__)
     # print number of objects
-    print (Warrior.num_of_warriors)
-    
+    print(Warrior.num_of_warriors)
+
     # Second Example
-    
-    monster = Monster(30,20)
+
+    monster = Monster(30, 20)
     print(monster.position())
     # change value of class variable
     Monster.set_move(5)
-    monster.move(10,10)
+    monster.move(10, 10)
     print(monster.position())
-    
+
     # using the "alternative constructor"
     second_monster = Monster.from_str("10-30")
     print(second_monster.__dict__)
-    
+
     # to test staticmethod
     now = datetime.datetime.now()
-    print (Monster.is_gameday(now))
+    print(Monster.is_gameday(now))
     
     
     

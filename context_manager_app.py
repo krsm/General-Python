479388851c3372811a96387e7c8e798f6ddd9e11
krsm <krsm184@gmail.com>
2017-01-28 17:43:20 -0500

    adding context manager study

--- context_manager_app.py
+++ context_manager_app.py
@@ -6,26 +6,57 @@ Simple study related to context-manager
 
 2. __enter__ is the before method and can return the value you catch with the as clause
 
-3. __exit__ is the after method and accepts three arguments (excpetion_type, execption_instance, traceback).
-It can return True to  supress exceptions that occur with block.
+3. __exit__ is the after method and accepts three arguments (exception_type, exception_instance, traceback).
+It can return True to  suppress exceptions that occur with block.
 
-4. __init__ shoud be used to catch arguments to your context-manager (filename to open, etc)
+4. __init__ should be used to catch arguments to your context-manager (filename to open, etc)
 
 """
 
-class ContextManager(object):
+from contextlib import contextmanager
+
+# First Example
+class MyContextManager(object):
+
+    # def print_msg(self, msg):
+    #     print(msg)
 
     def __init__(self):
-        print ("__init__")
+        print("__init__")
 
     #before stuff
     def __enter__(self):
         print("__enter__")
+        return self
 
-    def __exit__ (self, type, value, traceback):
-        print()
+    def __exit__(self, exc_type, value, traceback):
+        print(exc_type, value, traceback)
+
+
+
+# Second Example
+@contextmanager
+def file_open(path, mode = 'w'):
+    try:
+        file_obj = open(path, mode)
+        yield file_obj
+    except OSError:
+        print('Error')
+    finally:
+        print('Closing File')
+        file_obj.close()
 
 
 if __name__ == "__main__":
 
-    pass
+    # with MyContextManager() as test:
+    #     test.print_msg("Using context manager")
+    #     # raise ValueError
+
+    with file_open('test.txt') as fo:
+        fo.write('Context Managers')
+
+    with file_open('test.txt') as fo:
+        content = fo.read()
+
+    print(content)

f080541315c3ee034ae0e27cd5b4527d06802057
krsm <krsm184@gmail.com>
2017-01-28 17:43:44 -0500

    fixing indentation

--- Multithreading/stdy_threads.py
+++ Multithreading/stdy_threads.py
@@ -7,10 +7,12 @@ Simple study related do threads
 import _thread as thread
 import time
 
+
 # Example 1
 def son(tid):
 
     print("Executing son", tid)
+
 
 def father():
 
@@ -18,14 +20,14 @@ def father():
     while True:
         k = k + 1
         thread.start_new_thread(son, (k,))
-        if input() == "q":break
+        if input() == "q": break
 
 
 # Example 2
 def counter(myId, count):
     for i in range(count):
         time.sleep(1)
-        print('[%s] => %s' % (myId,i))
+        print('[%s] => %s' % (myId, i))
 
 if __name__ == "__main__":
 
@@ -34,7 +36,7 @@ if __name__ == "__main__":
 
     # Example 2
     for i in range(5):
-        thread.start_new_thread(counter,(i,5))
+        thread.start_new_thread(counter, (i, 5))
 
     time.sleep(5)
     print("Exiting main thread")

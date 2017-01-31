fb930ce532bbe04834beb6de3a962e990f3ffd46
krsm <krsm184@gmail.com>
2017-01-30 19:33:55 -0500

    fixing indentation

--- classes_propertydecorators.py
+++ classes_propertydecorators.py
@@ -4,36 +4,35 @@ Simple study related to property decorators
 
 """
 
-# First Example
 
+# First Example
 class Warrior(object):
     
     # delta position - class variable
     delta_position = 10
-    #number of warriors
+    # number of warriors
     num_of_warriors = 0
     
-    def __init__(self,posx,posy):
+    def __init__(self, posx, posy):
         self.posx = posx
         self.posy = posy
         
-        Warrior.num_of_warriors+=1
+        Warrior.num_of_warriors += 1
         
-    def move(self, posx,posy):
-        self.posx = posx+ self.delta_position
-        self.posy = posy +self.delta_position
+    def move(self, posx, posy):
+        self.posx = posx + self.delta_position
+        self.posy = posy + self.delta_position
         
     def position(self):
-        return '{} , {}' .format(self.posx,self.posy)
+        return '{} , {}' .format(self.posx, self.posy)
         
-    # define as a method and acessing as an attribute    
+    # define as a method and accessing as an attribute
     @property
-    def returnposition(self):
-        return '{} position x , {} position y' .format(self.posx,self.posy)
+    def return_position(self):
+        return '{} position x , {} position y' .format(self.posx, self.posy)
     
 
 if __name__ == "__main__":
     
-    warrior = Warrior(10,20)
-    print(warrior.returnposition)
-    
+    warrior = Warrior(10, 20)
+    print(warrior.return_position)

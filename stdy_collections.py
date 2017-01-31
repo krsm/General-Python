5eb31acde79bf52086cef77f81682365858301fa
krsm <krsm184@gmail.com>
2017-01-30 19:42:46 -0500

    added code related to collections - namedtuple

--- stdy_collections.py
+++ stdy_collections.py
@@ -6,9 +6,14 @@ simple study of collections
 import collections
 
 
-
+Person = collections.namedtuple('Person', 'name age gender')
 
 
 if __name__ == "__main__":
 
+    print('Type of person:', type(Person))
+    Jay = Person(name='Jay', age=30, gender='male')
+    Jane = Person(name='Jane', age=20, gender='female')
 
+    for person in [Jane, Jay]:
+        print(person.name, person.age, person.gender)

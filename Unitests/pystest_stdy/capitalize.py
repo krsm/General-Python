cdbad5880bba9bb41e284055f9fb5116ed8ca8bc
krsm <krsm184@gmail.com>
2017-02-14 18:17:48 -0500

    Adding TypeError verification

--- Unitests/pystest_stdy/capitalize.py
+++ Unitests/pystest_stdy/capitalize.py
@@ -6,6 +6,11 @@ Simple study related to pytest
 """
 
 
-def capitalize_string(str):
-    return str.capitalize()
+# it will receive a string and capitalize
+def capitalize_string(s):
+    if not isinstance(s, str):
+        raise TypeError('Please provide a string argument')
+    else:
+        return s.capitalize()
+
 

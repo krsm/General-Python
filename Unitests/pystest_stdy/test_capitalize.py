d7f7ae4da48d54497343031fd0140355f82001f9
krsm <krsm184@gmail.com>
2017-02-14 18:12:54 -0500

    Adding exception handler

--- Unitests/pystest_stdy/test_capitalize.py
+++ Unitests/pystest_stdy/test_capitalize.py
@@ -14,3 +14,10 @@ def test_capitalize_string():
     c = capitalize.capitalize_string('pytest')
     assert c == 'Pytest'
 
+
+# custom exception
+def test_raises_exception_string_argument():
+    # pytest.raises will raise a TypeError in case
+    # the argument passed is not a string
+    with pytest.raises(TypeError):
+        capitalize.capitalize_string(10)

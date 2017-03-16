8781d624306a697673de953c7f48ed9679acd845
krsm <krsm184@gmail.com>
2017-03-15 20:59:35 -0400

    changing indentation

--- decorators.py
+++ decorators.py
@@ -4,19 +4,24 @@ simple study of decorators
 
 """
 
+
 # First example
 def outerfunction(outer):
+    print("test")
     def innerfunction(inner):
         return outer(inner) + 1
     return innerfunction
+
 
 @outerfunction
 def addone(x):
     return x + 1
 
+
 # Second Example
 def wrapfunction(fn):
-    print("Executing wra_function")
+    print("Executing wrapfunction")
+
     def wrappedfunction():
         print("Calling %s inside wrappedfunction" % fn.__name__)
         # calling fn
@@ -24,14 +29,15 @@ def wrapfunction(fn):
         print("Executed %s inside wrappedfunction" % fn.__name__)
     return wrappedfunction
 
+
 @wrapfunction
 def tobedecorated():
     print("Executing tobedecorated function")
 
 if __name__ == "__main__":
-    
-    pass
-    #first example
-    #print(addone(5))
-    #second example
-    #tobedecorated()
+
+    # pass
+    # first example
+    # print(addone(5))
+    # second example
+    tobedecorated()
